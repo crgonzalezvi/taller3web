9d5d869f9f7dec1b66eaf2a4777a57a8594510bf
angalvezc <angalvezc@unal.edu.co>
2025-02-06 15:32:47 -0500

    ..

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,15 +21,18 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware('auth','role:estudiante')->group(function () {
-    Route::get('/estudiante', function (){
-        return "estudiante";
+Route::middleware('auth','role:student')->group(function () {
+    Route::get('students',[StudentController::class, 'index'])->name('students.index');
+    Route::post('students',[StudentController::class, 'store'])->name('students.store');
+    Route::get('students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+    Route::put('students/{id}', [StudentController::class, 'update'])->name('students.update');
+    Route::delete('students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
 
-    });
+
 });
-Route::middleware('auth','role:docente')->group(function () {
-    Route::get('/docente', function (){
-        return "docente";
+Route::middleware('auth','role:teacher')->group(function () {
+    Route::get('/teacher', function (){
+        return "teacher";
 
     });
 });
